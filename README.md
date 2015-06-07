19085e8db00e57c726b80217711460b918b49331
Mike McNeil <mike@balderdash.co>
2015-06-07 15:28:54 -0500

    Turned this back into a transform and used 'browserify-transform-tools' to get access to the machinepack key from the package.json file.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 <h1>
   <a href="http://node-machine.org" title="Node-Machine public registry"><img alt="node-machine logo" title="Node-Machine Project" src="http://node-machine.org/images/machine-anthropomorph-for-white-bg.png" width="50" /></a>
-  browserify-plugin-machinepack
+  browserify-transform-machinepack
 </h1>
 
 ### [Docs](http://node-machine.org/machinepack-math) &nbsp; [Browse other machines](http://node-machine.org/machinepacks) &nbsp;  [FAQ](http://node-machine.org/implementing/FAQ)  &nbsp;  [Newsgroup](https://groups.google.com/forum/?hl=en#!forum/node-machine)
 
-This package is a browserify plugin that makes machinepacks work when they are required from a browserified module (or each other).
+This package is a browserify transform that makes machinepacks work when they are required from a browserified module (or each other).
 
 
 ## License
--- index.js
+++ index.js
@@ -5,80 +5,44 @@
 var Path = require('path');
 var util = require('util');
 var _ = require('lodash');
-var through = require('through2');
+var transformTools = require('browserify-transform-tools');
 
 
-/**
- * browserify-plugin-machinepack
- *
- * @param  {BrowserifyTask} b
- */
-module.exports = function (b){
-  // console.log('b:\n', b, false, null);
-  console.log('basedir:\n', b._mdeps.basedir);
-  // console.log('opts:\n',opts);
-  var basedir = b._mdeps.basedir;
+module.exports = transformTools.makeStringTransform('machinepack', {}, function (code, transformOptions, done) {
+  if(!transformOptions.config) {
+    return done(new Error('Could not find `machinepack` object in package.json file.'));
+  }
 
-  var packMetadata;
-  b.on('package', function (pkg){
-    console.log('GOT PKG:',pkg);
-    packMetadata = pkg.machinepack;
-  });
+  var filePath = transformOptions.file;
+  var packPath = transformOptions.configData.configDir;
+  var packMetadata = transformOptions.config;
 
-  /**
-   * @param  {String} filePath
-   * @param  {Object} opts
-   * @return {Stream2}
-   */
-  b.transform(function (filePath, opts){
+  var isTopLevelPackIndex = _.endsWith(filePath, Path.join(packPath, 'index.js'));
 
-    // console.log('transforming file "%s" with opts:',filePath,opts);
+  // If this is not the `index.js` file of a machinepack, leave
+  // the code alone.
+  if (!isTopLevelPackIndex) {
+    return done(null, code);
+  }
 
-    // Use `through2` to build a nice little stream that will accumulate
-    // the code for us.
-    var code = '';
-    var stream;
-    stream = through.obj(function (buf, enc, next) {
-        code += buf.toString('utf8');
-        return next();
-    }, function onCodeReady(next) {
-        var isTopLevelPackIndex = _.endsWith(filePath, Path.join(basedir, 'index.js'));
+  // If this is a machinepack, replace the index.js file with
+  // a browserify-compatible version of the code that does not use
+  // Machine.pack() (because browserify doesn't know how to handle
+  // dynamic requires).
+  var shimCode = _.reduce(packMetadata.machines, function (memo, machineIdentity){
+    var line = util.format('  \'%s\': Machine.build( require(\'%s\') ),\n', machineIdentity, './'+Path.join(packMetadata.machineDir,machineIdentity));
+    memo += line;
+    return memo;
+  },
+  '// This shim was generated during browserification of this machinepack.\n'+
+  '// Because Machine.pack() uses dynamic require() calls, which is not supported \n'+
+  '// natively by browserify, the boilerplate index.js file in this pack was automatically\n'+
+  '// replaced with explicit requires of each machine herein.\n'+
+  'var Machine = require(\'machine\');\n'+
+  '\n'+
+  'module.exports = {\n');
+  shimCode += '};\n';
+  // console.log('\n------\n',shimCode,'\n\n');
 
-        // If this is not the `index.js` file of a machinepack, leave
-        // the code alone.
-        if (!isTopLevelPackIndex) {
-          this.push(new Buffer(code));
-          return next();
-        }
-
-        // TODO:
-        // Support other machinepacks required from inside the machines
-        // of the top-level pack.
-
-        // If this is a machinepack, replace the index.js file with
-        // a browserify-compatible version of the code that does not use
-        // Machine.pack() (because browserify doesn't know how to handle
-        // dynamic requires).
-        var shimCode = _.reduce(packMetadata.machines, function (memo, machineIdentity){
-          var line = util.format('  \'%s\': Machine.build( require(\'%s\') ),\n', machineIdentity, './'+Path.join(packMetadata.machineDir,machineIdentity));
-          memo += line;
-          return memo;
-        },
-        '// This shim was generated during browserification of this machinepack.\n'+
-        '// Because Machine.pack() uses dynamic require() calls, which is not supported \n'+
-        '// natively by browserify, the boilerplate index.js file in this pack was automatically\n'+
-        '// replaced with explicit requires of each machine herein.\n'+
-        'var Machine = require(\'machine\');\n'+
-        '\n'+
-        'module.exports = {\n');
-        shimCode += '};\n';
-        // console.log('\n------\n',shimCode,'\n\n');
-
-        this.push(new Buffer(shimCode));
-        return next();
-    });
-
-    // Provide our new stream to browserify
-    return stream;
-  });
-};
+  return done(null, shimCode);
+});
